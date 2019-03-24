e623d93088e27b062cfc6783d2c2801a5d151a6b
LeacOighear <tcryan6@gmail.com>
2019-03-24 13:07:22 +0000

    Create index.js

---
+++ birthday/index.js
@@ -0,0 +1,47 @@
+var startDate = new Date("Mar 24, 1993");
+// 
+var timer = setInterval(function() {
+
+    let now = new Date();
+    
+var years = diff_years(now, startDate)
+var days = diff_days(now, startDate)
+var hours = diff_hours(now, startDate)
+var mins =diff_minutes(now, startDate)
+
+    
+        document.getElementById("timer-years").innerHTML = years
+        document.getElementById("timer-days").innerHTML = days;
+        document.getElementById("timer-hours").innerHTML = hours
+        document.getElementById("timer-mins").innerHTML = mins
+}, 1000);
+
+function diff_years(dt2, dt1) 
+ {
+
+  var diff =(dt2.getTime() - dt1.getTime()) / 1000;
+  return Math.abs(Math.ceil(diff/ (60 * 60 * 24 * 365.25)));
+   
+ }
+
+ function diff_days(dt2, dt1) 
+ {
+
+  var diff =(dt2.getTime() - dt1.getTime()) / 1000;
+  return Math.abs(Math.floor(diff /= (60 * 60 * 24)));
+   
+ }
+ function diff_hours(dt2, dt1) 
+ {
+
+  var diff =(dt2.getTime() - dt1.getTime()) / 1000;
+  return Math.abs(Math.floor(diff /= (60 * 60)));
+   
+ }
+ function diff_minutes(dt2, dt1) 
+ {
+
+  var diff =(dt2.getTime() - dt1.getTime()) / 1000;
+  return Math.abs(Math.floor(diff /= 60));
+   
+ }
